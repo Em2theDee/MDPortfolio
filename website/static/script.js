fd54e18b04e0658c68448f8c3205606c287774af
Ubuntu <ubuntu@vps-37aef906.vps.ovh.net>
2023-11-08 19:04:08 +0000

    first commit

---
+++ website/static/script.js
@@ -0,0 +1,14 @@
+// scripts.js
+
+const cursor = document.querySelector('.custom-cursor');
+
+function updateCursorPosition(event) {
+    const cursorSize = 16; // Set the size of the cursor
+    const cursorOffsetX = cursorSize / 1;
+    const cursorOffsetY = cursorSize / 1;
+    
+    cursor.style.left = event.clientX - cursorOffsetX + 'px';
+    cursor.style.top = event.clientY - cursorOffsetY + 'px';
+}
+
+document.addEventListener('mousemove', updateCursorPosition);
--- website/views.py
+++ website/views.py
@@ -6,16 +6,3 @@ views = Blueprint("views", __name__)
 @views.route('/')
 def index():
     return render_template("index.html")
-    
-@views.route('/veg-box')
-def veg_box():
-    return render_template("veg_box.html")
-
-
-@views.route('dashboard')
-def dashboard():
-    print(session.get("admin"))
-    if session.get("admin"):
-        return render_template('dashboard.html')
-    else:
-        return redirect(url_for('auth.login'))
