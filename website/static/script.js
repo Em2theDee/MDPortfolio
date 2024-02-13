db2a2596b147849afd27bf0d656c803494c35c7f
Em2theDee <dariusdm27@gmail.com>
2024-02-13 16:29:55 +0000

    hello world

--- website/static/script.js
+++ website/static/script.js
@@ -1,18 +1,5 @@
 // scripts.js
 
-const cursor = document.querySelector('.custom-cursor');
-
-function updateCursorPosition(event) {
-    const cursorSize = 16; // Set the size of the cursor
-    const cursorOffsetX = cursorSize / 1;
-    const cursorOffsetY = cursorSize / 1;
-    
-    cursor.style.left = event.clientX - cursorOffsetX + 'px';
-    cursor.style.top = event.clientY - cursorOffsetY + 'px';
-}
-
-document.addEventListener('mousemove', updateCursorPosition);
-
 function handleWindowResize() {
     const height = window.innerHeight;
     const screenHeightElements = document.querySelectorAll('.screen-height');
@@ -23,32 +10,3 @@ function handleWindowResize() {
 }
 
 window.addEventListener('resize', handleWindowResize);
-
-document.addEventListener("DOMContentLoaded", function() {
-    var frontContainer = document.getElementById('front-container');
-    var imageUrl = 'Images/main comp.gif';
-
-    // Create an intersection observer
-    var observer = new IntersectionObserver(function(entries, observer) {
-        entries.forEach(function(entry) {
-            if (entry.isIntersecting) {
-                // Load the image when it comes into view
-                loadImage(imageUrl, function() {
-                    frontContainer.style.backgroundImage = 'url(' + imageUrl + ')';
-                });
-                observer.unobserve(frontContainer);
-            }
-        });
-    }, { rootMargin: '0px 0px 100px 0px' }); // Adjust rootMargin as needed
-
-    // Start observing the element
-    observer.observe(frontContainer);
-});
-
-function loadImage(url, callback) {
-    var img = new Image();
-    img.onload = function() {
-        callback();
-    };
-    img.src = url;
-}
